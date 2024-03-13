c0a78a86ded8095bd54f6a546a204827be563821
Diyor <ccdiyorrf@gmail.com>
2024-03-13 23:33:58 +0500

    Feature: Add / Update Event template

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,7 +1,13 @@
 const users_controller = {
     index: async (req, res) =>{
         res.render('');
-    }  
+    },
+    add: async (req, res) =>{
+        res.render('./add_update');
+    },
+    update: async (req, res) =>{
+        res.render('./add_update');
+    }
 };
   
 module.exports = users_controller;
--- routes/users/index.js
+++ routes/users/index.js
@@ -4,5 +4,7 @@ const users_controller = require('../../controllers/users');
 
 // localhost:3000/ (GET request)
 router.get('/', users_controller.index);
+router.get('/add', users_controller.add);
+router.get('/update', users_controller.update);
 
 module.exports = router;
